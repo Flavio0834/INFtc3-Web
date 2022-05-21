3f6e8afa91591e2b72934e30f785a4431507af4a
Flavio <flaviosestu@yahoo.fr>
2022-05-21 16:14:34 +0200

    Formatted files TD2-s9.py and VolcanDB.py

--- TD2/TD2-s9.py
+++ TD2/TD2-s9.py
@@ -1,69 +1,73 @@
 import http.server
 import socketserver
-from VolcanDB import *
+from VolcanDB import VolcanDB
 
 # Create personalised class to handle HTTP requests
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     # Add a static path
-    static_dir='client/'
+    static_dir = "client/"
     # Override the do_GET method
     def do_GET(self):
-        if self.path.startswith('/time'):
+        if self.path.startswith("/time"):
             self.send_time()
-        elif 'css' in self.path:
-            self.path='css/'+self.path.split('/')[-1]
+        elif "css" in self.path:
+            self.path = "css/" + self.path.split("/")[-1]
             super().do_GET()
-        elif self.path.startswith('/volcans'):
+        elif self.path.startswith("/volcans"):
             self.send_volcans()
-        elif self.path.startswith('/volcan'):
-            volcan=self.path.split('/')[2]
+        elif self.path.startswith("/volcan"):
+            volcan = self.path.split("/")[2]
             self.send_volcan(volcan)
         else:
-            self.path=self.static_dir+self.path
+            self.path = self.static_dir + self.path
             super().do_GET()
 
-    def send(self,body,headers=[]):
+    def send(self, body, headers=[]):
         # on encode la chaine de caractères à envoyer
-        encoded = bytes(body, 'UTF-8')
+        encoded = bytes(body, "UTF-8")
 
         # on envoie la ligne de statut
         self.send_response(200)
 
         # on envoie les lignes d'entête et la ligne vide
         [self.send_header(*t) for t in headers]
-        self.send_header('Content-Length',int(len(encoded)))
+        self.send_header("Content-Length", int(len(encoded)))
         self.end_headers()
 
         # on envoie le corps de la réponse
         self.wfile.write(encoded)
 
     def send_time(self):
-        self.send("Voici l'heure du serveur : \n\n"+self.date_time_string(),[('Content-type','text/plain;charset=utf-8')])
+        self.send(
+            "Voici l'heure du serveur : \n\n" + self.date_time_string(),
+            [("Content-type", "text/plain;charset=utf-8")],
+        )
 
     def send_volcans(self):
-        db=VolcanDB("volcans/volcans.db")
-        self.send(db.get_list_of_volcanos(),[('Content-type','text/plain;charset=utf-8')])
+        db = VolcanDB("volcans/volcans.db")
+        self.send(
+            db.get_list_of_volcanos(), [("Content-type", "text/plain;charset=utf-8")]
+        )
 
-    def send_volcan(self,name):
-        db=VolcanDB("volcans/volcans.db")
-        infos=db.get_infos_of_volcano(name)
+    def send_volcan(self, name):
+        db = VolcanDB("volcans/volcans.db")
+        infos = db.get_infos_of_volcano(name)
         if infos:
-            infos=infos.split('\n')
-            res='<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<link rel="stylesheet" href="TD2-s9.css">\n\t</head>\n\t<body>\n'
-            res+="\t\t<h1>"+infos[1][6:]+"</h1>\n"
-            res+="\t\t<ul>\n"
-            for i in range(len(infos)-1):
-                info=infos[i].split(':',1)
-                res+=f"\t\t\t<li><b>{info[0]}</b>: {info[1]}</li>\n"
-            res+="\t\t</ul>\n"
-            res+="\t</body>\n</html>"
-            self.send(res,[('Content-type','text/html;charset=utf-8')])
+            infos = infos.split("\n")
+            res = '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<link rel="stylesheet" href="TD2-s9.css">\n\t</head>\n\t<body>\n'
+            res += "\t\t<h1>" + infos[1][6:] + "</h1>\n"
+            res += "\t\t<ul>\n"
+            for i in range(len(infos) - 1):
+                info = infos[i].split(":", 1)
+                res += f"\t\t\t<li><b>{info[0]}</b>: {info[1]}</li>\n"
+            res += "\t\t</ul>\n"
+            res += "\t</body>\n</html>"
+            self.send(res, [("Content-type", "text/html;charset=utf-8")])
         else:
-            self.send_error(404,message="Volcan "+name+" non trouvé")
+            self.send_error(404, message="Volcan " + name + " non trouvé")
 
     def send_css(self):
         pass
-        
 
 
 PORT = 8080
--- TD2/VolcanDB.py
+++ TD2/VolcanDB.py
@@ -1,75 +1,99 @@
 import sqlite3
 import json
 
+
 class VolcanDB:
-	def __init__(self,nomDB):
-		self.__conn=sqlite3.connect(nomDB)
-	def __del__(self):
-		self.__conn.close()
+    def __init__(self, nomDB):
+        self.__conn = sqlite3.connect(nomDB)
 
-	def get_number_of_volcanos(self):
-		try:
-			self.__cursor=self.__conn.cursor()
-			self.__cursor.execute("SELECT COUNT(*) FROM volcans")
-			return "There are "+str(self.__cursor.fetchone()[0])+" volcanos in the database."
-		except Exception as err:
-			print(err)
-			return None
+    def __del__(self):
+        self.__conn.close()
 
-	def get_list_of_volcanos(self):
-		try:
-			self.__cursor=self.__conn.cursor()
-			self.__cursor.execute("SELECT name,lat,lon FROM volcans")
-			volcanos=self.__cursor.fetchall()
-			res={}
-			i=1
-			for volcano in volcanos:
-				res[str(i)]={"name":volcano[0],"lat":volcano[1],"lon":volcano[2]}
-				i+=1
-			return json.dumps(res)
-		except Exception as err:
-			print(err)
-			return None
+    def get_number_of_volcanos(self):
+        try:
+            self.__cursor = self.__conn.cursor()
+            self.__cursor.execute("SELECT COUNT(*) FROM volcans")
+            return (
+                "There are "
+                + str(self.__cursor.fetchone()[0])
+                + " volcanos in the database."
+            )
+        except Exception as err:
+            print(err)
+            return None
 
-	def get_localisation_by_name(self,name):
-		try:
-			self.__cursor=self.__conn.cursor()
-			self.__cursor.execute("SELECT lat,lon FROM volcans WHERE name=?",(name,))
-			lat,lon=self.__cursor.fetchone()
-			return name+" is located at lat="+str(lat)+", lon="+str(lon)
-		except Exception as err:
-			print(err)
-			return None
+    def get_list_of_volcanos(self):
+        try:
+            self.__cursor = self.__conn.cursor()
+            self.__cursor.execute("SELECT name,lat,lon FROM volcans")
+            volcanos = self.__cursor.fetchall()
+            res = {}
+            i = 1
+            for volcano in volcanos:
+                res[str(i)] = {"name": volcano[0], "lat": volcano[1], "lon": volcano[2]}
+                i += 1
+            return json.dumps(res)
+        except Exception as err:
+            print(err)
+            return None
 
-	def get_volcanos_erupted_after(self,year):
-		try:
-			self.__cursor=self.__conn.cursor()
-			self.__cursor.execute("SELECT name FROM volcans WHERE eruption_year>=?",(year,))
-			volcanos=self.__cursor.fetchall()
-			return "Volcanos erupted after "+str(year)+" : "+str([k[0] for k in volcanos])
-		except Exception as err:
-			print(err)
-			return None
+    def get_localisation_by_name(self, name):
+        try:
+            self.__cursor = self.__conn.cursor()
+            self.__cursor.execute("SELECT lat,lon FROM volcans WHERE name=?", (name,))
+            lat, lon = self.__cursor.fetchone()
+            return name + " is located at lat=" + str(lat) + ", lon=" + str(lon)
+        except Exception as err:
+            print(err)
+            return None
 
-	def get_infos_of_volcano(self,name):
-		try:
-			self.__cursor=self.__conn.cursor()
-			self.__cursor.execute("SELECT * FROM volcans WHERE name=?",(name,))
-			infos=self.__cursor.fetchone()
-			columns=['volcano','name','wiki','elevation','lat','lon','eruption_date','eruption_year','abstract','photo']
-			res=""
-			for i in range(len(columns)):
-				res+=f"{columns[i]}: {infos[i]}\n"
-			return res
-		except Exception as err:
-			print(err)
-			return None
+    def get_volcanos_erupted_after(self, year):
+        try:
+            self.__cursor = self.__conn.cursor()
+            self.__cursor.execute(
+                "SELECT name FROM volcans WHERE eruption_year>=?", (year,)
+            )
+            volcanos = self.__cursor.fetchall()
+            return (
+                "Volcanos erupted after "
+                + str(year)
+                + " : "
+                + str([k[0] for k in volcanos])
+            )
+        except Exception as err:
+            print(err)
+            return None
+
+    def get_infos_of_volcano(self, name):
+        try:
+            self.__cursor = self.__conn.cursor()
+            self.__cursor.execute("SELECT * FROM volcans WHERE name=?", (name,))
+            infos = self.__cursor.fetchone()
+            columns = [
+                "volcano",
+                "name",
+                "wiki",
+                "elevation",
+                "lat",
+                "lon",
+                "eruption_date",
+                "eruption_year",
+                "abstract",
+                "photo",
+            ]
+            res = ""
+            for i in range(len(columns)):
+                res += f"{columns[i]}: {infos[i]}\n"
+            return res
+        except Exception as err:
+            print(err)
+            return None
 
 
-if __name__=='__main__':
-	db=VolcanDB("volcans/volcans.db")
-	print(db.get_number_of_volcanos())
-	print(db.get_localisation_by_name("Maipo"))
-	print(db.get_volcanos_erupted_after(1922))
-	print(db.get_list_of_volcanos())
-	print(db.get_infos_of_volcano("Acamarachi"))
+if __name__ == "__main__":
+    db = VolcanDB("volcans/volcans.db")
+    print(db.get_number_of_volcanos())
+    print(db.get_localisation_by_name("Maipo"))
+    print(db.get_volcanos_erupted_after(1922))
+    print(db.get_list_of_volcanos())
+    print(db.get_infos_of_volcano("Acamarachi"))
