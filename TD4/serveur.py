396e6aa777f37f9a81514035c9a0e2e9431a4fdd
maxchebib <maxime.chebib@ecl21.ec-lyon.fr>
2022-06-14 09:21:04 +0200

    Done Q3.4

--- TD4/serveur.py
+++ TD4/serveur.py
@@ -43,6 +43,10 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
         elif len(self.path_info) > 1 and self.path_info[0] == entity_name:
             self.send_data(self.path_info[1])
 
+        # Sinon si le chemin d'accès commence par /commentaires/entity_name on renvoie le commentaire
+        elif len(self.path_info) > 1 and self.path_info[0] == "commentaires":
+            self.send_commentaires_json()
+
         # ou pas...
         else:
             self.send_static()
@@ -187,6 +191,20 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def send_json(self, data):
         headers = [("Content-Type", "application/json")]
         self.send(json.dumps(data), headers)
+
+    def send_commentaires_json(self):
+        """Envoie la liste des commentaires d'un point d'intérêt au format json"""
+
+        entity = self.path_info[1]
+        c = conn.cursor()
+        c.execute("SELECT * FROM commentaires WHERE site = ?",(entity,))
+        data = c.fetchall()
+
+        body = json.dumps([dict(d) for d in data])  # réponse en json
+        headers = [
+            ("Content-Type", "application/json")
+        ]  # précise au client quel type de données il doit traiter
+        self.send(body, headers)
 
     #
     # On envoie les infos d'une entité
